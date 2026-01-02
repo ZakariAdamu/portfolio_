c209db22c705d9d9b62b54a6655bdea18c85f8b0
Zakari <zakari.adamu714@gmail.com>
2026-01-02 01:09:10 +0100

    feat: add testimonials section

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./sections/HeroSection";
 import { AboutSection } from "./sections/AboutSection";
+import { TestimonialsSection } from "./sections/TestimonialsSection";
 
 const PortfolioContent = () => {
 	return (
 		<>
 			<HeroSection />
 			<AboutSection />
+			<TestimonialsSection />
 			{/* <ProjectsSection />
     <ContactSection /> */}
 		</>
